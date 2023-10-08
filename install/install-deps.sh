afd1538663185d5e4ac10b9b90bb354df7db4be7
avi <avixiii@outlook.com>
2023-10-09 03:14:22 +0700

    initial commit

---
+++ install/install-deps.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+#if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+#    source ./install-deps-linux.sh
+#elif [[ "$OSTYPE" == "darwin"* ]]; then
+#    source ./install-deps-macos.sh
+#fi
+
+git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.8.0
+source $HOME/.asdf/asdf.sh
+
+asdf plugin add nodejs
+NODEJS_CHECK_SIGNATURES=no asdf install nodejs 14.17.0
+asdf global nodejs $(asdf list nodejs | tail -1 | sed 's/^ *//g')
+
+npm install -g git-branch-utils
+npm install -g neovim
